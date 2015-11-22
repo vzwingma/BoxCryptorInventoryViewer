523823943a277ebe9e2d59bdb1e7363ccc2c92a6
Vincent ZWINGMANN <vincent.zwingmann@gmail.com>
2015-11-22 17:55:57 +0100

    Ajout de la double commande --generate --view

--- BoxCryptorListGenerator/main/java/com/terrier/boxcryptor/BoxCryptorGenerator.java
+++ BoxCryptorListGenerator/main/java/com/terrier/boxcryptor/BoxCryptorGenerator.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.nio.charset.Charset;
 import java.util.Calendar;
-import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -17,6 +16,7 @@ import org.yaml.snakeyaml.Yaml;
 import com.terrier.boxcryptor.objects.BCInventaireFichier;
 import com.terrier.boxcryptor.objects.BCInventaireRepertoire;
 import com.terrier.boxcryptor.objects.BCInventoryItemView;
+import com.terrier.boxcryptor.objects.InventoryCommandEnum;
 
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
@@ -38,11 +38,11 @@ public class BoxCryptorGenerator extends Application {
 	private File repertoireChiffre;
 	// Répertoire non chiffré
 	private File repertoireNonChiffre;
-	
+
 	private Calendar startTraitement = Calendar.getInstance();
-	
+
 	private static final String INVENTORY_FILENAME = "liste_Fichiers_BoxCryptor.yml";
-	
+
 	private static BCInventaireRepertoire inventaireNew;
 	/**
 	 * Classe main
@@ -50,23 +50,46 @@ public class BoxCryptorGenerator extends Application {
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
-		
+
+		// Forcage en UTF-8 pour les caractères chinois utilisés par BC
 		System.setProperty("file.encoding","UTF-8");
 		Field charset = Charset.class.getDeclaredField("defaultCharset");
 		charset.setAccessible(true);
 		charset.set(null,null);
-		
-		BoxCryptorGenerator bcx = new BoxCryptorGenerator();
 
-		System.out.println("Début de la génération de l'inventaire");
-		if(bcx.getPaths(args)){
-			inventaireNew = bcx.generateInventory();
+		InventoryCommandEnum commande = null;
+		if(args.length >= 1){
+			String cmd = args[0];
+			if(cmd.equalsIgnoreCase("--generate")){
+				commande = InventoryCommandEnum.GENERATE;
+			}
+			else if(cmd.equalsIgnoreCase("--view")){
+				commande = InventoryCommandEnum.VIEW;
+			}
 		}
+
+
+		if(InventoryCommandEnum.GENERATE.equals(commande)){
+			BoxCryptorGenerator bcx = new BoxCryptorGenerator();
+			System.out.println("Début de la génération de l'inventaire");
+			if(bcx.getPaths(args)){
+				inventaireNew = bcx.generateInventory();
+			}
+			else{
+				System.err.println("******** ERREUR : Les répertoires sont incorrects ********");
+			}
+			System.out.println("Fin de la génération de l'inventaire");
+		}
+		else if(InventoryCommandEnum.VIEW.equals(commande)){
+			// launch();
+			System.out.println("To be developped");
+		}
+		// Résultat commande inconnue
 		else{
-			System.err.println("******** ERREUR : Les répertoires sont incorrects ********");
+			System.out.println("**** USAGE ****");
+			System.out.println(" -- generate [Répertoire chiffré (D:)] [Répertoire non chiffré (X:)]");
+			System.out.println(" -- view [Répertoire non chiffré]");
 		}
-		System.out.println("Fin de la génération de l'inventaire");
-	//	launch();
 	}
 
 	/**
@@ -74,12 +97,12 @@ public class BoxCryptorGenerator extends Application {
 	 * @throws Exception 
 	 */
 	public BCInventaireRepertoire generateInventory() throws Exception{
-		
+
 		// Lecture de l'inventaire
 		BCInventaireRepertoire inventaire = getFileInventory();
 		System.out.println(inventaire.getCleMap());
 		printDelayFromBeginning("Read file Inventory");
-		
+
 		// Création de l'inventaire
 		ExecutorService threadsPool = Executors.newFixedThreadPool(100);
 		DirectoryInventoryStreamGeneratorCallable inventory = new DirectoryInventoryStreamGeneratorCallable(
@@ -90,8 +113,8 @@ public class BoxCryptorGenerator extends Application {
 		BCInventaireRepertoire inventaireNew = inventory.call();
 		threadsPool.shutdown();
 		printDelayFromBeginning("Generate Inventory");
-		
-		
+
+
 		// Ecriture de l'inventaire
 		writeInventory(inventaireNew);
 		printDelayFromBeginning("Dump Inventory");
@@ -109,7 +132,7 @@ public class BoxCryptorGenerator extends Application {
 		Scanner entree =   new Scanner(System.in, "UTF-8");
 		String cheminChiffre = "";
 		if(args != null && args.length >= 1){
-			cheminChiffre = args[0];
+			cheminChiffre = args[1];
 		}
 		else{
 			System.out.println("Entrez le répertoire CHIFFRE (D:)");
@@ -121,7 +144,7 @@ public class BoxCryptorGenerator extends Application {
 
 		String cheminNonChiffre = "";
 		if(args != null && args.length >= 2){
-			cheminNonChiffre = args[1];
+			cheminNonChiffre = args[2];
 		}
 		else{
 			System.out.println("Entrez le répertoire NON CHIFFRE (X:)");
@@ -147,10 +170,10 @@ public class BoxCryptorGenerator extends Application {
 		BCInventaireRepertoire repertoire;
 		if(inventoryFile.exists()){
 			System.out.println("Enregistrement de la liste dans " + inventoryFile.getCanonicalPath());
-			
+
 			Yaml yml = new Yaml();
 			repertoire = yml.loadAs(new FileInputStream(inventoryFile), BCInventaireRepertoire.class);
-			
+
 		}
 		else{
 			System.out.println("Le fichier "+ inventoryFile.getAbsolutePath()+ " n'existe pas. Création du fichier");
@@ -160,23 +183,23 @@ public class BoxCryptorGenerator extends Application {
 	}
 
 
-	
+
 	/**
 	 * Ecriture de l'inventaire (dump)
 	 * @param inventaireR inventaire
 	 * @throws IOException
 	 */
 	private void writeInventory(final BCInventaireRepertoire inventaireR) throws IOException{
-		
+
 		Yaml yml = new Yaml();
 		FileWriter inventoryWriter = new FileWriter(new File(this.repertoireNonChiffre.getAbsolutePath(), INVENTORY_FILENAME));
 		yml.dump(inventaireR, inventoryWriter);
 		inventoryWriter.flush();
 		inventoryWriter.close();
-		
+
 	}
-	
-	
+
+
 	/**
 	 * @param traitement
 	 */
@@ -184,41 +207,41 @@ public class BoxCryptorGenerator extends Application {
 		System.out.println("["+traitement+"] > " + (Calendar.getInstance().getTimeInMillis() - startTraitement.getTimeInMillis())  + " ms");
 	}
 
-	
 
-	
-	
+
+
+
 	/* (non-Javadoc)
 	 * @see javafx.application.Application#start(javafx.stage.Stage)
 	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Inventory Viewer");        
-	
-		
+
+
 		TreeItem<BCInventoryItemView> rootItem  = viewDirectoryInventory(inventaireNew);
 		TreeView<BCInventoryItemView> tree = new TreeView<BCInventoryItemView> (rootItem);
 		tree.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TreeItem<BCInventoryItemView>>()
-        {
-            @Override
-            public void changed(ObservableValue<? extends TreeItem<BCInventoryItemView>> observable, TreeItem<BCInventoryItemView> oldValue, TreeItem<BCInventoryItemView> newValue)
-            {
-            	
-            	newValue.getValue().alternateMessageView();
-            	System.out.println("Alternate : " + newValue.getValue().toString());
-            }
-        });
-		
-		
-		
+		{
+			@Override
+			public void changed(ObservableValue<? extends TreeItem<BCInventoryItemView>> observable, TreeItem<BCInventoryItemView> oldValue, TreeItem<BCInventoryItemView> newValue)
+			{
+
+				newValue.getValue().alternateMessageView();
+				System.out.println("Alternate : " + newValue.getValue().toString());
+			}
+		});
+
+
+
 		StackPane root = new StackPane();
 		root.getChildren().add(tree);
 		primaryStage.setScene(new Scene(root, 500, 550));
 		primaryStage.show();
-		
+
 	}
-	
-	
+
+
 	/**
 	 * View directory inventaire
 	 * @param inventaireRepertoire  inventaireRépertoire
---
+++ BoxCryptorListGenerator/main/java/com/terrier/boxcryptor/objects/InventoryCommandEnum.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.terrier.boxcryptor.objects;
+
+/**
+ * @author vzwingma
+ *
+ */
+public enum InventoryCommandEnum {
+	GENERATE, VIEW;
+}
